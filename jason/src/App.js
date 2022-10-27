b63287781e24ccd1433cf32b16315e45b2911038
Jason Di <jasondisj@gmail.com>
2022-10-27 16:09:34 -0700

    Import components

--- jason/src/App.js
+++ jason/src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
