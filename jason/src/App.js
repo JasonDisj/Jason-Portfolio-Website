e327cdac9309c4a26fe1b9f81d96cdfc041b1a74
Jason Di <jasondisj@gmail.com>
2022-10-28 10:40:38 -0700

    Add more components

--- jason/src/App.js
+++ jason/src/App.js
@@ -5,9 +5,12 @@ import About from "./components/about/About";
 import Services from "./components/services/Services";
 import Resume from "./components/resume/Resume";
 import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
-  return <div>App</div>;
+  return <></>;
 };
 
 export default App;
