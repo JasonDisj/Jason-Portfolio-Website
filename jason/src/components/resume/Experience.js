f16c907fd02737adac71c5d723ac5b8c172404c8
Jason Di <jasondisj@gmail.com>
2022-10-31 16:49:59 -0700

    Data of experiences

---
+++ jason/src/components/resume/Experience.js
@@ -0,0 +1,52 @@
+const Experience = [
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2007 - 2011",
+    title: "Bachelor - Public Affairs Administration",
+    description: "Shanghai University of Electric Power ",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2016 - 2018",
+    title: "Post-Degree Diploma - Marketing Management",
+    description: "Langara College",
+  },
+  {
+    id: 3,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2022 - 2022",
+    title: "Diploma - Full Stack Web Development",
+    description: "Lighthouse Labs",
+  },
+  {
+    id: 4,
+    category: "work",
+    icon: "icon-briefcase",
+    year: "2009 - 2015",
+    title: "Wedding Planner",
+    description: "Shanghai Bao Hui Wedding Planning Ltd.",
+  },
+  {
+    id: 5,
+    category: "work",
+    icon: "icon-briefcase",
+    year: "2012 - 2015",
+    title: "Sales Manager",
+    description: "Shanghai Bao Hui Wedding Planning Ltd.",
+  },
+  {
+    id: 6,
+    category: "work",
+    icon: "icon-briefcase",
+    year: "2018 - 2022",
+    title: "Digital Printing Machine Operator",
+    description: "Oddball Workshop Inc.",
+  },
+];
+
+export default Experience;
