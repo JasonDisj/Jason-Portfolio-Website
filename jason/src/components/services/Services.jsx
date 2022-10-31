e82bbac280e505d9c1ce6ec00017a5602415ab25
Jason Di <jasondisj@gmail.com>
2022-10-31 14:17:28 -0700

    Services component

--- jason/src/components/services/Services.jsx
+++ jason/src/components/services/Services.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./services.css";
+import Image1 from "../../assets/Web-Development.png";
+import Image2 from "../../assets/UI-Design.png";
 
 const Services = () => {
-  return <div>Services</div>;
+  const data = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Web Development",
+      description: "Web Development",
+    },
+    {
+      id: 2,
+      image: Image2,
+      title: "UI/UX Design",
+      description: "UI Design",
+    },
+  ];
+
+  return (
+    <section className="services container section" id="services">
+      <h2 className="section__title">Services</h2>
+      <div className="services__container grid">
+        {data.map((eachData) => {
+          return (
+            <div className="services__card" key={eachData.id}>
+              <img src={eachData.image} alt="" className="services__img" />
+              <h3 className="services__title">{eachData.title}</h3>
+              <p className="services__description">{eachData.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
