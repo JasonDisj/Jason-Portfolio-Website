03d7e23d369f800a6ac7e3777d8535bc4c633af9
Jason Di <jasondisj@gmail.com>
2022-11-01 14:39:52 -0700

    Add project description and github link

--- jason/src/components/portfolio/project.js
+++ jason/src/components/portfolio/project.js
@@ -1,29 +1,43 @@
 import Project1 from "../../assets/Realtime-Chat-App.png";
 import Project2 from "../../assets/Relic.png";
-import Project3 from "../../assets/Schedular.png";
+import Project3 from "../../assets/Scheduler.png";
 import Project4 from "../../assets/MariosList.png";
 
-const project = [
+const Project = [
   {
     id: 1,
     image: Project1,
     title: "Realtime-Chat",
+    category: "App",
+    description: "Real-time Chat App with React, Node, Socket.IO ⚡",
+    link: "https://github.com/JasonDisj/Realtime-Chat-App",
   },
   {
     id: 2,
     image: Project2,
     title: "Relic",
+    category: "App",
+    description: `A React based Tinder clone project that users can distract the pesky ghosts living in their houses by setting them up on a "date" to avoid the heeby jeebies 👻`,
+    link: "https://github.com/JasonDisj/ghost-tindr",
   },
   {
     id: 3,
     image: Project3,
-    title: "Schedular",
+    title: "Interview Scheduler",
+    category: "App",
+    description:
+      "A single-page app which allows users to book, edit, and cancel appointments with a list of interviewers.",
+    link: "https://github.com/JasonDisj/scheduler",
   },
   {
     id: 4,
     image: Project4,
     title: "Mario's List",
+    category: "E-commerce",
+    description:
+      "A mock version of craigslist or Kijiji where users can put video games up for sale, search, delete, and mark items as sold.",
+    link: "https://github.com/JasonDisj/lighthouse-midterm-project",
   },
 ];
 
-export default project;
+export default Project;
