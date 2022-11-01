665f8c33f3821fccbadbbaf6a7494a238727c86a
Jason Di <jasondisj@gmail.com>
2022-11-01 14:49:00 -0700

    Show different projects based on category with useState

--- jason/src/components/portfolio/Portfolio.jsx
+++ jason/src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
+import Project from "./Project";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const [items, setItems] = useState(Project);
+  const filterItem = (categoryItem) => {
+    const updatedItems = Project.filter((currentItem) => {
+      return currentItem.category === categoryItem;
+    });
+
+    setItems(updatedItems);
+  };
+
+  return (
+    <section className="project container section" id="project">
+      <h2 className="section__title">Recent Projects</h2>
+
+      <div className="project__filters">
+        <span className="project__item">Everything</span>
+        <span
+          className="project__item"
+          onClick={() => {
+            filterItem("App");
+          }}
+        >
+          App
+        </span>
+        <span
+          className="project__item"
+          onClick={() => {
+            filterItem("E-commerce");
+          }}
+        >
+          E-commerce
+        </span>
+      </div>
+
+      <div className="project__container grid">
+        {items.map((item) => {
+          return (
+            <div className="project__card" key={item.id}>
+              <div className="project__thumnail">
+                <img src={item.image} alt="" className="project__image" />
+                <div className="project__mask"></div>
+              </div>
+
+              <span className="project__category">{item.category}</span>
+              <h3 className="project__title">{item.title}</h3>
+              <h3 className="project__description">{item.description}</h3>
+              <a href={item.link} className="project__button">
+                <i className="icon-link project__button-icon"></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
