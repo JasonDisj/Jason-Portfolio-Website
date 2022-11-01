4f8fb2636dc9fad0f049fde4aa56ac9a24803adb
Jason Di <jasondisj@gmail.com>
2022-11-01 10:41:28 -0700

    Add data of projects that I've completed

---
+++ jason/src/components/portfolio/project.js
@@ -0,0 +1,29 @@
+import Project1 from "../../assets/Realtime-Chat-App.png";
+import Project2 from "../../assets/Relic.png";
+import Project3 from "../../assets/Schedular.png";
+import Project4 from "../../assets/MariosList.png";
+
+const project = [
+  {
+    id: 1,
+    image: Project1,
+    title: "Realtime-Chat",
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: "Relic",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Schedular",
+  },
+  {
+    id: 4,
+    image: Project4,
+    title: "Mario's List",
+  },
+];
+
+export default project;
