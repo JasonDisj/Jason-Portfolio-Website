3cad9c5b1b59474689e6491544be9d374e7c32a7
Jason Di <jasondisj@gmail.com>
2022-11-01 10:49:40 -0700

    Update Experience from experience.js

--- jason/src/components/resume/Resume.jsx
+++ jason/src/components/resume/Resume.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./resume.css";
+import Experience from "./experience";
+import Card from "./Card";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <section className="resume container section" id="resume">
+      <h2 className="section__title">Experience</h2>
+      <div className="resume__container grid">
+        <div className="timeline gird">
+          {Experience.map((eachExperience) => {
+            if (eachExperience.category === "education") {
+              return (
+                <Card
+                  key={eachExperience.id}
+                  icon={eachExperience.icon}
+                  title={eachExperience.title}
+                  year={eachExperience.year}
+                  desc={eachExperience.description}
+                />
+              );
+            }
+          })}
+        </div>
+
+        <div className="timeline gird">
+          {Experience.map((eachExperience) => {
+            if (eachExperience.category === "work") {
+              return (
+                <Card
+                  key={eachExperience.id}
+                  icon={eachExperience.icon}
+                  title={eachExperience.title}
+                  year={eachExperience.year}
+                  desc={eachExperience.description}
+                />
+              );
+            }
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
