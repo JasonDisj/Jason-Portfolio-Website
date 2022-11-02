73c01896391621e3cb47a8e7b34cfa9471b40c4d
Jason Di <jasondisj@gmail.com>
2022-11-02 10:32:06 -0700

    Contact component

--- jason/src/components/contact/Contact.jsx
+++ jason/src/components/contact/Contact.jsx
@@ -2,7 +2,65 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact container section" id="contact">
+      <h2 className="section__title">Get In Touch</h2>
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title">Let's talk!</h3>
+          <p className="contact__details">
+            Please send me an email{" "}
+            <img
+              src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif"
+              width="28px"
+              alt="hi"
+            ></img>
+          </p>
+        </div>
+
+        <form action="" className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Insert your name"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                placeholder="Insert your email"
+              />
+            </div>
+          </div>
+
+          <div className="contact__form-div">
+            <input
+              type="text"
+              className="contact__form-input"
+              placeholder="Insert your subject"
+            />
+          </div>
+
+          <div className="contact__form-div contact__form-area">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="contact__form-input"
+              placeholder="Write your message"
+            ></textarea>
+          </div>
+
+          <button className="btn">Send</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
