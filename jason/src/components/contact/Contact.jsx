57ccf26b0d9d3ad7d1eadb99dd1e929fdbfb236c
Jason Di <jasondisj@gmail.com>
2022-11-09 14:25:05 -0800

    Update contact title

--- jason/src/components/contact/Contact.jsx
+++ jason/src/components/contact/Contact.jsx
@@ -29,15 +29,15 @@ const Contact = () => {
       <h2 className="section__title">Get In Touch</h2>
       <div className="contact__container grid">
         <div className="contact__info">
-          <h3 className="contact__title">Let's talk!</h3>
-          <p className="contact__details">
-            Please send me an email{" "}
+          <h3 className="contact__title">
+            Let's connect{" "}
             <img
               src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif"
               width="28px"
               alt="hi"
             ></img>
-          </p>
+          </h3>
+          <p className="contact__details">Please send me an email</p>
         </div>
 
         <form action="" className="contact__form" onSubmit={sendEmail}>
