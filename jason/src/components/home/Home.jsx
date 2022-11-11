f593f214c07603502ecad3802e03299ab1186846
Jason Di <jasondisj@gmail.com>
2022-11-11 10:15:04 -0800

    Update description

--- jason/src/components/home/Home.jsx
+++ jason/src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Jason Di</h1>
-        <span className="home__education">I'm a Full-Stack developer</span>
+        <span className="home__education">I'm a Full-Stack Developer</span>
 
         <HeaderSocials />
 
